3f38a81d55aed98a16eb789f79b504689ed73181
Sam Swindler <samswindler@gmail.com>
2024-02-27 13:21:19 -0700

    Draft a nuke DB script

---
+++ milestone_3/remove_all_data.sql
@@ -0,0 +1,23 @@
+DELETE FROM "main"."Wears";
+
+DELETE FROM "main"."Equips";
+
+DELETE FROM "main"."Completes";
+
+DELETE FROM "main"."Character";
+
+DELETE FROM "main"."Account";
+
+DELETE FROM "main"."UnlocksWeapon";
+
+DELETE FROM "main"."UnlocksArmor";
+
+DELETE FROM "main"."UnlocksAccessory";
+
+DELETE FROM "main"."Advancement";
+
+DELETE FROM "main"."Accessory";
+
+DELETE FROM "main"."Armor";
+
+DELETE FROM "main"."Weapon";
