cdef6b1404b9d5334bc0bcab9c3392b94e68d698
Sam Swindler <samswindler@gmail.com>
2024-03-14 14:04:19 -0600

    Drafting up more of the subviews

--- milestone_4/create_views.sql
+++ milestone_4/create_views.sql
@@ -13,26 +13,60 @@ CREATE VIEW Wields AS
 SELECT CharId, WeaponId FROM Character
 
 -- 1
-CREATE VIEW accessories_are_equipped AS
+CREATE VIEW accessories_equipped AS
 SELECT * FROM Accessory, Equips
 WHERE Accessory.AccessoryId = Equips.AccessoryId;
 
 -- 2
-CREATE VIEW weapons_are_equipped
+CREATE VIEW weapons_equipped
 SELECT * FROM Weapon, Wields
 WHERE Weapon.WeaponId = Wields.WeaponId;
 
 -- 3
-CREATE VIEW armors_are_equipped
+CREATE VIEW armors_equipped
 SELECT * FROM Armor, Wears
 WHERE Armor.ArmorId = Wears.ArmorId;
 
 -- The VIEWs that Rename everything to be Union Compatible
 -- 4
+CREATE VIEW weapons_equipped_unionable
+    SELECT WeaponId AS ItemId,
+        'Weapon' AS ItemType,
+        WeaponName AS ItemName,
+        WeaponDesc AS ItemDesc,
+        ImageURL,
+        ObtainMethod,
+        StatAttack,
+        NULL AS StatBonus
+        NULL AS StatDefense
+    FROM weapons_are_equipped;
 
 -- 5
+CREATE VIEW accessories_equipped_unionable
+    SELECT AccessoryId AS ItemId,
+        'Accessory' AS ItemType,
+        AccessoryName AS ItemName,
+        AccessoryDesc AS ItemDesc,
+        ImageURL,
+        ObtainMethod,
+        NULL AS StatAttack,
+        StatBonus,
+        NULL AS StatDefense
+    FROM Accessory;
 
 -- 6
+CREATE VIEW armors_equipped_unionable
+    SELECT ArmorId AS ItemId,
+        'Armor' AS ItemType,
+        ArmorName AS ItemName,
+        ArmorDesc AS ItemDesc,
+        ImageURL,
+        ObtainMethod,
+        NULL AS StatAttack,
+        NULL AS StatBonus,
+        StatDefense
+    FROM Armor;
+
 
 -- The VIEW that UNIONs all the items into a single table
 
