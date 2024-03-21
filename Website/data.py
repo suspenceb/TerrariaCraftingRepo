5684df0c564bf4fde4542a22e245e911a5d0ef2d
rootbeerefloat <ethanbeere123@gmail.com>
2024-03-21 13:04:24 -0600

    Created Data.py and added login/logout functions

---
+++ Website/data.py
@@ -0,0 +1,76 @@
+import mysql.connector
+from dotenv import load_dotenv
+import os
+import hashlib
+
+# ----------------- Begin Helper Functions ----------------- #
+
+def get_db_connection():
+    load_dotenv()
+    conn = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_DATABASE")
+    )
+    return conn
+
+# ------------------ End Helper Functions ------------------ #
+
+# --------------------- Begin Endpoints -------------------- #
+
+
+# Post Login Function - Use this to log in a user
+# Inputs: username (string), password (string)
+# Outputs: token (string) or None
+def post_login(username, password):
+    # Connect to database and establish cursor
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # Hash the password
+    hashed_password = hashlib.sha256(password.encode()).hexdigest()
+    
+    # Query the database
+    query = "SELECT UserId, Username FROM Account WHERE Username = %s AND PasswordHash = %s"
+    cursor.execute(query, (username, hashed_password))
+    
+    # If a user is not returned, return None
+    user = cursor.fetchone()
+    if user is None:
+        return None
+    
+    # Generate a token for the user and post it to the database
+    token = hashlib.sha256(os.urandom(64)).hexdigest()
+    query = "INSERT INTO UserSession (UserId, Token) VALUES (%s, %s)"
+    print(str(user[0]) + " " + str(token))
+    cursor.execute(query, (str(user[0]), str(token)))
+    conn.commit()
+
+    # Close the connection and return the token
+    conn.close()
+    return token
+
+
+# Delete Login Function - Use this to log out a user
+# Inputs: token (string)
+# Outputs: True if successful, False if not
+def delete_login(token):
+    # Connect to database and establish cursor
+    conn = get_db_connection()
+    cursor = conn.cursor()
+
+    # Ensure the token exists
+    query = "SELECT Token FROM UserSession WHERE Token = %s"
+    cursor.execute(query, (token, ))
+    if cursor.fetchone() is None:
+        return False
+
+    # Query the database
+    query = "DELETE FROM UserSession WHERE Token = %s"
+    cursor.execute(query, (token, ))
+    conn.commit()
+
+    # Close the connection and return 1 if successful
+    conn.close()
+    return True
