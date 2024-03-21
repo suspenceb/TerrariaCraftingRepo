4ad60e7dd4ae989c88e12437eb7fc89664d370ac
Matthew Gregg <mgregg99@byu.edu>
2024-03-21 16:18:21 -0600

    Add Comments

--- Website/main.py
+++ Website/main.py
@@ -32,27 +32,38 @@ def index():
 
 @app.route("/account", methods=["GET", "POST"])
 def account():
+    # Check if the user is logged in
     if request.cookies.get("token") is None:
         return redirect(url_for("login"))
+    # Get the user from the database
     user = get_loggedin_user(request.cookies.get("token"))
     
     if request.method == "POST":
+        # Get the password and confirm password from the form
         password = request.form["inputPassword"]
         comfirm_password = request.form["inputPasswordconfirm"]
+
+        # Check if the passwords match
         if password != comfirm_password:
+            # If they don't match, flash a message and return the user to the account page
             flash("Passwords do not match")
         
+        # Update the password in the database
         update_password(user["userId"], password)
 
     return render_template("account.html", user=user)
 
 @app.route("/logout")
 def logout():
+    # Check if the user is logged in
     if request.cookies.get("token") is None:
+        # Redirect to the login page if the user is not logged in
         return redirect(url_for("login"))
     
+    # Delete the login from the database
     delete_login(request.cookies.get("token"))
     
+    # Redirect to the login page
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
