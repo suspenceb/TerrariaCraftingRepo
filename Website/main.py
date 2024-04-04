f093d0cf29ff361c8144d064bd3476be40b9f555
rootbeerefloat <ethanbeere123@gmail.com>
2024-04-04 13:00:31 -0600

    Changed port from 5000 to 8001

--- Website/main.py
+++ Website/main.py
@@ -55,6 +55,7 @@ def index():
         # Get the latest filters from the cookie
         
         # Acquire the list of items as a result of the filters
+        print()
 
 
 @app.route("/account", methods=["GET", "POST"])
@@ -113,4 +114,4 @@ def logout():
     return redirect(url_for("login"))
 
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(debug=True, host="0.0.0.0", port=8001)
