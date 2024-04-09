c6900a80aad59968b4bd9719a77450a1d1dcff64
rootbeerefloat <ethanbeere123@gmail.com>
2024-04-09 14:14:44 -0600

    Implemented error handling for equip

--- Website/data.py
+++ Website/data.py
@@ -336,7 +336,7 @@ def post_equipment(characterId: int, itemType: str, itemId: int) -> bool:
         query = "SELECT AccessoryId FROM Accessory WHERE AccessoryId = %s"
     cursor.execute(query, (itemId, ))
     if cursor.fetchone() is None:
-        return False
+        return "Item does not exist."
     
     # Ensure armor slots are free
     if itemType.lower() == "armor":
@@ -363,13 +363,13 @@ def post_equipment(characterId: int, itemType: str, itemId: int) -> bool:
         cursor.execute(query, (characterId, ))
         accessoryCount = cursor.fetchone()[0]
         if accessoryCount >= 6:
-            return False
+            return "Character already has 6 accessories equipped."
         
         # Check if the accessory is already equiped
         query = "SELECT AccessoryId FROM Equips WHERE CharId = %s AND AccessoryId = %s"
         cursor.execute(query, (characterId, itemId))
         if cursor.fetchone() is not None:
-            return False
+            return "Accessory already equipped."
 
     # Set the item
     if itemType.lower() == "weapon":
